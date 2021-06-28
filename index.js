03779c436b39d1a86ce1ae902cb3973073b6fc3d
Lex Nelson <lexinelson597@gmail.com>
2021-06-28 18:42:39 -0400

    commit tree e8bd78f24f799661f6171293760d3b42f5d749d7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+/*function wrapAdjective(modifier='*'){
+    const partOne = 'You are'
+    innerFunction(adjective='special') {
+        console.log(`${partOne} ${modifier}${adjective}${modifier}`);
+    }
+}
+
+const encouragingAdjectiveFunction = wrapAdjective(modifier='*') {
+    innerFunction(example='special'){
+        return `You are ${modifier}${example}`
+    }
+}*/
+
+function wrapAdjective(flair='*'){
+    return function(adjective='special') 
+    {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
